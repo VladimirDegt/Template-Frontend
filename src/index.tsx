88a63228b5352d0d841117c4fce207f743432565
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-11-27 14:55:43 +0200

    create Theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import { render } from 'react-dom';
-import cls from './index.module.scss'
-render(
-    
-    <div className={cls.div}><span className={cls.span}>перший реакт компонент</span></div>,
-    document.getElementById("root")
-    
-)
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
+import ThemeProvider from "./theme/ThemeProvider";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+    <BrowserRouter>
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
+    </BrowserRouter>
+);
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <p>About</p>
+        </div>
+    )
+}
+
+export default AboutPage;
