86ed807f0effe75a2c0a28aaf736dc40d4f2887b
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-12-01 08:57:45 +0200

    create page not found and loader

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { NotFoundPage } from "pages/NotFoundPage";
 import { RouteProps } from "react-router-dom";
 
 export enum AppRouters{
     MAIN = 'main',
-    ABOUT = 'about'
+    ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 };
 
 export const RoutePath: Record<AppRouters, string> = {
     [AppRouters.MAIN]: '/',
-    [AppRouters.ABOUT]: '/about'
+    [AppRouters.ABOUT]: '/about',
+    [AppRouters.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRouters, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRouters, RouteProps> = {
     [AppRouters.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage/>
-    }
+    },
+        [AppRouters.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage/>
+    },
 }
