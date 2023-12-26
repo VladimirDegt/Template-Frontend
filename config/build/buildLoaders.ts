1aa2679911e2797fa2abb582cfc541f6d2e2caf7
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-12-26 16:50:03 +0200

    install react-toasts

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,7 +10,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     };
 
 	const cssLoader = {
-		test: /\.s[ac]ss$/i,
+		test: /\.(s[ac]ss|css)$/i,
 		use: [
 			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
 			{
--- src/features/DragAndDrop/ui/DragAndDrop.tsx
+++ src/features/DragAndDrop/ui/DragAndDrop.tsx
@@ -4,6 +4,10 @@ import React, {memo, useState} from 'react';
 import {validateDropFile} from "shared/lib/validateDropFile/validateDropFile";
 import UploadFileIcon from 'shared/assets/icons/uploadFile.svg'
 import {Text, TextTheme} from "shared/ui/Text/Text";
+import {Button, ButtonSize, ButtonTheme} from "shared/ui/Button/Button";
+import {$api} from "shared/api/api";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface DragAndDropProps {
     className?: string;
@@ -11,6 +15,7 @@ interface DragAndDropProps {
 export const DragAndDrop = memo(({ className }: DragAndDropProps) => {
     const [drag, setDrag] = useState(false);
     const [nameFile, setNameFile] = useState('');
+    const [file, setFile] = useState<null | Blob>(null);
     function dragStartHandler(e: React.DragEvent): void {
         e.preventDefault()
         setDrag(true)
@@ -32,43 +37,91 @@ export const DragAndDrop = memo(({ className }: DragAndDropProps) => {
         }
 
         setNameFile(`${files[0].name}`)
-        const formData: FormData = new FormData();
-        formData.append('emailTable', files[0])
+        setFile(files[0])
+    }
+
+    function onClickBtnClear() {
+        setDrag(false)
+        setNameFile('Файл не обрано')
+        toast("Wow so easy!");
+    }
+
+    async function onClickBtnSend(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault();
+
+        if(file !== null) {
+            const formData: FormData = new FormData();
+            formData.append('emailTable', file);
+
+            try {
+                const response = await $api.post('/drag/sendFile', formData)
+                if (!response.data) {
+                    console.error('помилка')
+                }
+            } catch (e) {
+                console.error('помилка')
+            }
+
+            setFile(null);
+            setDrag(false);
+            setNameFile('');
+            return
+        }
+        return
     }
 
     return (
         <div className={classNames(cls.app, {}, [className])}>
+            <ToastContainer />
             <label htmlFor="file" className={classNames(cls['container-label'])}>
                 {drag
                     ?
-                    <div
-                        className={classNames(cls['drop-area'])}
-                        onDragStart={e => dragStartHandler(e)}
-                        onDragLeave={e => dragLeaveHandler(e)}
-                        onDragOver={e => dragStartHandler(e)}
-                        onDrop={e => onDropHandler(e)}
-                    >
-                        <UploadFileIcon/>
-                        <Text title='Відпустити файл для завантаження'/>
-                    </div>
+                        <div
+                            className={classNames(cls['drop-area'])}
+                            onDragStart={e => dragStartHandler(e)}
+                            onDragLeave={e => dragLeaveHandler(e)}
+                            onDragOver={e => dragStartHandler(e)}
+                            onDrop={e => onDropHandler(e)}
+                        >
+                            <UploadFileIcon/>
+                            <Text title='Відпустити файл для завантаження'/>
+                        </div>
                     :
-                    <div
-                        className={classNames(cls['drop-area'])}
-                        onDragStart={e => dragStartHandler(e)}
-                        onDragLeave={e => dragLeaveHandler(e)}
-                        onDragOver={e => dragStartHandler(e)}
-                    >
-                        <UploadFileIcon/>
-                        <Text title='Натисніть або перетягніть файл для завантаження'/>
-                    </div>
+                        <div
+                            className={classNames(cls['drop-area'])}
+                            onDragStart={e => dragStartHandler(e)}
+                            onDragLeave={e => dragLeaveHandler(e)}
+                            onDragOver={e => dragStartHandler(e)}
+                        >
+                            <UploadFileIcon/>
+                            <Text title='Натисніть або перетягніть файл для завантаження'/>
+                        </div>
                 }
 
             </label>
-            {nameFile
-                ? <Text title={nameFile}/>
-                : <Text title={'Файл не обрано'} />
-            }
-
+            <div className={cls.containerBtn}>
+                <Button
+                    theme={ButtonTheme.OUTLINE}
+                    size={ButtonSize.L}
+                    type="submit"
+                    className={cls.btnSend}
+                    onClick={onClickBtnSend}
+                >
+                    <Text title={'Відправити'}/>
+                </Button>
+                {nameFile
+                    ? <Text title={nameFile}/>
+                    : <Text title={'Файл не обрано'}/>
+                }
+                <Button
+                    theme={ButtonTheme.OUTLINE}
+                    size={ButtonSize.L}
+                    className={cls.btnSend}
+                    onClick={onClickBtnClear}
+                >
+                    <Text title={'Очистити'} theme={TextTheme.ERROR}/>
+                </Button>
+            </div>
             <input
                 type="file"
                 id="file"
