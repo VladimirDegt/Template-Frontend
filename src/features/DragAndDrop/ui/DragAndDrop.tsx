9cd5bc3b491c3021dcc40be6a3c39d6e2d0ad70e
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-12-26 13:33:38 +0200

    add styles

--- src/features/DragAndDrop/ui/DragAndDrop.tsx
+++ src/features/DragAndDrop/ui/DragAndDrop.tsx
@@ -1,13 +1,16 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './DragAndDrop.module.scss';
-import React, { memo, useState } from 'react';
+import React, {memo, useState} from 'react';
 import {validateDropFile} from "shared/lib/validateDropFile/validateDropFile";
+import UploadFileIcon from 'shared/assets/icons/uploadFile.svg'
+import {Text, TextTheme} from "shared/ui/Text/Text";
 
 interface DragAndDropProps {
     className?: string;
 }
 export const DragAndDrop = memo(({ className }: DragAndDropProps) => {
     const [drag, setDrag] = useState(false);
+    const [nameFile, setNameFile] = useState('');
     function dragStartHandler(e: React.DragEvent): void {
         e.preventDefault()
         setDrag(true)
@@ -24,32 +27,53 @@ export const DragAndDrop = memo(({ className }: DragAndDropProps) => {
         setDrag(false);
 
         if(!validateDropFile(files)) {
+            setNameFile('Файл не обрано')
             return
         }
 
+        setNameFile(`${files[0].name}`)
         const formData: FormData = new FormData();
         formData.append('emailTable', files[0])
     }
 
     return (
-        <div className={classNames(cls.app, { }, [className])}>
-            {drag
-                ? <div
+        <div className={classNames(cls.app, {}, [className])}>
+            <label htmlFor="file" className={classNames(cls['container-label'])}>
+                {drag
+                    ?
+                    <div
                         className={classNames(cls['drop-area'])}
                         onDragStart={e => dragStartHandler(e)}
                         onDragLeave={e => dragLeaveHandler(e)}
                         onDragOver={e => dragStartHandler(e)}
                         onDrop={e => onDropHandler(e)}
-                >Відпустити файл для завантаження
-                </div>
-                : <div
+                    >
+                        <UploadFileIcon/>
+                        <Text title='Відпустити файл для завантаження'/>
+                    </div>
+                    :
+                    <div
                         className={classNames(cls['drop-area'])}
                         onDragStart={e => dragStartHandler(e)}
                         onDragLeave={e => dragLeaveHandler(e)}
                         onDragOver={e => dragStartHandler(e)}
-                >Перетягнити файл щоб завантажити
-                </div>
+                    >
+                        <UploadFileIcon/>
+                        <Text title='Натисніть або перетягніть файл для завантаження'/>
+                    </div>
+                }
+
+            </label>
+            {nameFile
+                ? <Text title={nameFile}/>
+                : <Text title={'Файл не обрано'} />
             }
+
+            <input
+                type="file"
+                id="file"
+                className={cls.fileInput}
+            />
         </div>
     );
 })
