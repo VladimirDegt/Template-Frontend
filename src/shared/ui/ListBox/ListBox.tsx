86aaf769330e05e8c82ad9c4a8d6e1e66c799f01
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-01-04 16:58:15 +0200

    Update ListBox.tsx

--- src/shared/ui/ListBox/ListBox.tsx
+++ src/shared/ui/ListBox/ListBox.tsx
@@ -2,6 +2,7 @@ import {Fragment, useState} from 'react';
 import { Listbox as HListBox } from '@headlessui/react';
 import cls from './ListBox.module.scss'
 import {classNames} from "@/shared/lib/classNames/classNames";
+import {Button, ButtonSize, ButtonTheme} from "@/shared/ui/Button/Button";
 
 const people = [
     { id: 1, name: 'Без даних' },
@@ -18,7 +19,19 @@ export function ListBox() {
             value={selectedPerson}
             onChange={setSelectedPerson}
         >
-            <HListBox.Button className={cls.btnSend}>{selectedPerson.name}</HListBox.Button>
+            <HListBox.Button
+                as={'div'}
+                className={cls.btnSend}
+            >
+                <Button
+                    theme={ButtonTheme.OUTLINE}
+                    size={ButtonSize.L}
+                    className={cls.btnSend}
+                >
+                    {selectedPerson.name}
+                </Button>
+
+            </HListBox.Button>
             <HListBox.Options className={cls.options}>
                 {people.map((person) => (
                     <HListBox.Option
