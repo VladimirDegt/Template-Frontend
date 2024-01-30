12d2ac4b85436a1b45373390d1dadb4d1acee85d
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-01-30 16:55:57 +0200

    fix: theme

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -28,7 +28,7 @@ export const Modal = (props: ModalProps) => {
     const [isClosing, setIsClosing] = useState(false);
     const [isMounted, setIsMounted] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
-    const { _ } = useTheme();
+    const { theme } = useTheme();
 
     useEffect(() => {
         if(isOpen){
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -9,7 +9,7 @@ interface ThemeSwitcherProps {
 }
 
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
-    const { _, toggleTheme } = useTheme();
+    const { theme, toggleTheme } = useTheme();
     
     return (
         <Button
